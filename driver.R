1a05c835316f705d90fd9e58b000266c73b3ab9a
Charles Tilley <ctilley@mathematica-mpr.com>
2021-01-06 20:53:23 -0500

    init: driver to interact with functions and sources

---
+++ driver.R
@@ -0,0 +1,33 @@
+rm(list = ls())
+
+# package declarations
+library(assertr)
+library(dplyr)
+library(rvest)
+library(stringr)
+library(tibble)
+library(xml2)
+
+source("fn/gr_meta_pull.R")
+
+
+# load IDs
+ids <-
+  readr::read_csv("src/input.csv") %>%
+  assertr::verify(length(unique(id))==nrow(.)) %>%
+  pull(id)
+
+
+# pull down metadata
+df <- NULL
+
+for (id in c(ids)) {
+  
+  Sys.sleep(base::sample(c(5:10), size = 1))
+  
+  df <- rbind(df, gr_meta_pull(id))
+  
+}
+
+df
+
