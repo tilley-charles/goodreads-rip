8d177281f4adc629421372a3e253d3719d709426
Charles Tilley <tilley.charles@gmail.com>
2023-02-06 08:16:35 -0500

    refactor: update html tags for new site layout

--- fn/gr_meta_pull.R
+++ fn/gr_meta_pull.R
@@ -12,11 +12,11 @@ gr_meta_pull <- function(id) {
   df <-
     tibble(
       gr_id      = id,
-      gr_title   = html %>% rvest::html_node("#bookTitle")   %>% html_text(.),
-      gr_author  = html %>% rvest::html_node("#aboutAuthor") %>% html_node("[class='bookAuthorProfile__name']") %>% html_text(.),
-      gr_pages   = html %>% rvest::html_node("#details")     %>% html_node("[itemprop='numberOfPages']")        %>% html_text(.),
-      gr_ratings = html %>% rvest::html_node("#bookMeta")    %>% html_node("[itemprop='ratingCount']")          %>% html_attr("content"),
-      gr_stars   = html %>% rvest::html_node("#bookMeta")    %>% html_node("[itemprop='ratingValue']")          %>% html_text(.)
+      gr_title   = html %>% rvest::html_node("[class='BookPageTitleSection__title']") %>% html_nodes("h1") %>% html_text(.),
+      gr_author  = html %>% rvest::html_node("[class='ContributorLink__name']") %>% html_text(.),
+      gr_pages   = html %>% rvest::html_node("[class='FeaturedDetails']") %>% html_node("p:nth-child(1)") %>% html_text(.),
+      gr_ratings = html %>% rvest::html_node("[class='RatingStatistics__meta']") %>% html_node("span:nth-child(1)") %>% html_text(.),
+      gr_stars   = html %>% rvest::html_node("[class='RatingStatistics__rating']") %>% html_text(.)
     )
   
   df <-
